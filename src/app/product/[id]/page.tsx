3b43f9aa92931c4bedff4e6d70d03ef466d862f6
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2024-06-25 16:46:11 -0300

    feat: add single page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,42 @@
-export default function ProductPage({ params }: { params: { id: string } }) {
-  return <div>Produc IDt: {params.id}</div>
+import { ProductType } from "@/types/ProductType";
+import Image from "next/image";
+
+async function getSingleProducts(id: string) {
+  try {
+    const response = await fetch(
+      `https://dummyjson.com/products/${id}?select=id,title,price,description,thumbnail,category`
+    );
+    const data = await response.json();
+    const dataproduct = data;
+    return dataproduct;
+  } catch (error) {
+    console.error("Erro:", error);
+    return null;
+  }
+}
+
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const productsList: ProductType = await getSingleProducts(params.id);
+  const { id, title, price, description, thumbnail, category } = productsList;
+
+  return (
+    <>
+      <h1>{id}</h1>
+      <h1>{title}</h1>
+      <h1>{price}</h1>
+      <h1>{description}</h1>
+      <h1>{thumbnail}</h1>
+      <Image
+        src={thumbnail}
+        alt="image"
+        width={200}
+        height={200}
+        className="w-[auto] h-[200px]"
+      />
+    </>
+  );
 }
