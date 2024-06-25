03880d2c0062593ced104ee40b80533001b0bdfb
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2024-06-25 15:43:50 -0300

    feat: add component vitrine

--- src/app/components/ProductItem.tsx
+++ src/app/components/ProductItem.tsx
@@ -1,5 +1,6 @@
 import { ProductType } from "@/types/ProductType";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type ProductProps = {
@@ -9,28 +10,31 @@ type ProductProps = {
 export default function ProductItem({ product }: ProductProps) {
   return (
     <>
-      <div key={product.id}>
-        <div className="flex justify-center w-full mb-4">
-          <Image
-            src={product.thumbnail}
-            alt="image"
-            width={200}
-            height={200}
-            className="w-[auto] h-[200px]"
-          />
-        </div>
-        <div className="mb-3">
-          <h4>{product.title}</h4>
-          <span>
-            {
-              new Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL' }).format(
-                product.id,
-              )
-            }
+      <Link href={`/product/${product.id}`}>
+        <div key={product.id}>
+          <div className="flex justify-center w-full mb-4">
+            <Image
+              src={product.thumbnail}
+              alt="image"
+              width={200}
+              height={200}
+              className="w-[auto] h-[200px]"
+            />
+          </div>
+          <div className="mb-3">
+            <h4>{product.title}</h4>
+            <span>
+              {new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(product.id)}
             </span>
+          </div>
+          <button className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Adicionar ao carrinho
+          </button>
         </div>
-        <button className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Adicionar ao carrinho</button>
-      </div>
+      </Link>
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
+import VitrineProducts from "./components/VitrineProducts/VitrineProducts";
 
 export default function Home() {
   return (
@@ -7,7 +8,10 @@ export default function Home() {
     <Header />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <ProductList/>  
+        <ProductList/>
+        <VitrineProducts title='Camisetas Masculinas' categoryName="mens-shirts" limit={4}/>  
+        <VitrineProducts title='Beleza' categoryName="beauty" limit={4}/>  
+        <VitrineProducts title='Sapatos Femininos' categoryName="womens-shoes" limit={4}/>  
       </div>
     </main>
     </>
