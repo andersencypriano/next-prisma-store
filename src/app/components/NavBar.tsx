3ca56560f08ef78d219a9e79cb27506a884b9169
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2024-06-26 12:30:08 -0300

    feat: add links menu

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,24 +1,53 @@
+'use-client'
 import Link from "next/link";
 import React from "react";
+import ProductItem from "./ProductItem";
 
-type Props = {};
+async function getCategoryList() {
+  try {
+    const response = await fetch(
+      `https://dummyjson.com/products/category-list?limit=5`
+    );
+    const data = await response.json();
+    const productsArray = data;
+    return productsArray;
+  } catch (error) {
+    console.error("Erro:", error);
+    return null;
+  }
+}
 
-export default function NavBar({}: Props) {
+export default async function NavBar() {
+  const productsList = await getCategoryList();
+
   return (
     <>
+      {/* <ul className="flex gap-2 justify-between">
+        {productsList.map((categoryName: string) => (
+          <li key={categoryName}>
+            <Link href={`/category/${categoryName}`}>{`${categoryName}`}</Link>
+          </li>
+        ))}
+      </ul> */}
+
       <ul className="flex gap-2 justify-between">
         <li>
-          <Link href="#">Camisetas</Link>
+          <Link href='/category/beauty'>Beauty</Link>
+        </li>
+        
+        <li>
+          <Link href="/category/motorcycle">Motorcycle</Link>
         </li>
         <li>
-          <Link href="#">Blusas</Link>
+          <Link href="/category/sunglasses">Sunglasses</Link>
         </li>
         <li>
-          <Link href="#">Calças</Link>
+          <Link href="/category/tablets">Tablets</Link>
         </li>
         <li>
-          <Link href="#">Tênis</Link>
+          <Link href="/category/tops">Tops</Link>
         </li>
+        
       </ul>
     </>
   );
