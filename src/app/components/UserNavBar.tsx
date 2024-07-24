b65603473fd2ee81123f0d82dedeeb57d25231a8
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2024-07-24 12:31:58 -0300

    feat: add clerk auth

--- src/app/components/UserNavBar.tsx
+++ src/app/components/UserNavBar.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import MiniCart from './Cart/MiniCart'
+import React from "react";
+import MiniCart from "./Cart/MiniCart";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { PiUserCircleLight } from "react-icons/pi";
 
-type Props = {}
+type Props = {};
 
 export default function UserNavBar({}: Props) {
   return (
     <>
-      <MiniCart/>
+      <div className="flex items-center gap-3">
+        <MiniCart />
+        <SignedOut>
+          <SignInButton mode="modal">
+            <PiUserCircleLight size={29} title="Fazer login"/>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </>
-  )
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptPT } from "@clerk/localizations";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Drawer from "./components/Cart/Drawer/Drawer";
+
 export const metadata: Metadata = {
   title: "MT Next Store.",
   description: "",
@@ -17,13 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} overflow-x-hidden`}>
-        <SpeedInsights/>
-        <Drawer/>
-        <Header />
-        {children}
+    <ClerkProvider localization={ptPT}>
+      <html lang="en">
+        <body className={`${inter.className} overflow-x-hidden`}>
+          <SpeedInsights />
+          <Drawer />
+          <Header />
+          {children}
         </body>
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
